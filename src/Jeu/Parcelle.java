7a6be3b87ef63b862fb351e44a55e795d4259fac
frederic-prochnow <frederic.prochnow@etudiant.univ-lille1.fr>
2016-03-08 18:17:49 +0100

    COMMIT-08-03-2016
    COMMIT DU 08 MARS 2016

---
+++ src/Jeu/Parcelle.java
@@ -0,0 +1,28 @@
+package Jeu;
+
+public class Parcelle {
+	int type;
+
+	
+	Parcelle (int type) {
+		if(type>=0 && type<11) {
+			this.type = type;
+		} else {
+			this.type = -1;
+		}
+	}
+
+	public String toString() {
+		if(type == -1) {return " ";}
+		else if(type == 0) {return "e";}
+		else if(type == 1) {return "v";}
+		else if(type == 2) {return "n";}
+		else if(type == 3) {return "E";}
+		else if(type == 4) {return "V";}
+		else if(type == 5) {return "N";}
+		else if(type == 6) {return "R";}
+		else if(type == 7) {return "C";}
+		else if(type == 8) {return "~";}
+		else  { return "";}
+	} 
+}
