dfc6d37049e73e3a34a082c47289c63174d3a3b1
Frédéric PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-04-02 16:02:32 +0200

    Update Personnage.java

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,6 +1,15 @@
+/**
+ * Importation de class
+ */
 import javax.swing.JOptionPane;
-
+/**
+ * Class Personnage
+ * Class de création et de gestion des personnages
+ */
 public class Personnage {
+	/**
+	 * attribut
+	 */
 	protected String nom;
 	protected char symboleNom;
 	protected int type; // M�me que dans parcelle
@@ -11,7 +20,11 @@ public class Personnage {
 	private boolean detientClef;
 	private boolean detientTresor;
 
-	
+	/**
+	 * Constructeur de la class Personnage
+	 * Crée un personnage
+	 * @params String nom, int nbequipe, int nvenergie, Position p, int type
+	 */
 	Personnage(String nom, int equipe, int energie, Position p, int type){
 		this.nom = nom;
 		this.energie = energie;
@@ -27,27 +40,50 @@ public class Personnage {
 		}
 	}
 	
+	/**
+	 * Méthode qui retourne le type de l'élément
+	 * @return int type
+	 */
 	public int getType(){
 		return this.type;
 	}
 	
+	/**
+	 * Méthode qui retire le nb d'énregie souhaiter
+	 * @params int energie
+	 */
 	public void perdEnergie(int energie){
 		this.energie -= energie;
 	}
 	
+	/**
+	 * Méthode qui retourne la position du personnage
+	 * @return position p
+	 */
 	public Position getPos(){
 		return this.pos;
 	}
 	
+	/**
+	 * Méthode d'application de la position du personnage
+	 * @params Position p
+	 */
 	public void setPos(Position p){
 		this.pos = p;
 	}
 	
+	/**
+	 * Méthode qui retourne le nom du personnage
+	 * @return String nom
+	 */
 	public String getNom(){
 		return nom;
 	}
 	
-	// retourne le type du navire selon le personnage
+	/**
+	 * Méthode qui retourne le type du navire selon le personnage
+	 * @return int type
+	 */
 	public int getNavireType(){
 		if(equipe == 1){
 			return 2;
@@ -56,49 +92,97 @@ public class Personnage {
 		}
 	}
 	
+	/**
+	 * Méthode qui retourne le symbole de l'élément
+	 * @return char symbole
+	 */
 	public char getSymbole(){
 		return symboleNom;
 	}
 	
+	/**
+	 * Méthode qui return l'équipe
+	 * @return int equipe
+	 */
 	public int getEquipe(){
 		return equipe;
 	}
 	
+	/**
+	 * Méthode qui retournele niveau d'énergie
+	 * @return int energie
+	 */
 	public int getEnergie(){
 		return energie;
 	}
 	
+	/**
+	 * Méthode qui retourne un boolean signifiant si le personnage est sur le navire ou pas
+	 * @return boolean
+	 */
 	public boolean getSurnavire(){
 		return this.surNavire;
 	}
 	
+	/**
+	 * Méthode d'application sur le boolean gérant la position du personnage sur le navire
+	 * @params boolean
+	 */
 	public void setSurnavire(boolean b){
 		this.surNavire = b;
 	}
 	
+	/**
+	 * Méthode qui retourne un boolean signifiant si le personnage détient la clef du coffre
+	 * @return boolean
+	 */
 	public boolean getDetientClef(){
 		return this.detientClef;
 	}
 	
+	/**
+	 * Méthode d'application sur le bollean gérant l'aquisation de la clef par le personnage
+	 * @params boolean
+	 */
 	public void setDetientClef(boolean set) {
 		this.detientClef = set;
 	}
 	
+	/**
+	 * Méthode qui retourne le bollean signifiant si e personnage a en sa possesion le tresor
+	 * @return boolean
+	 */
 	public boolean getDetientTresor(){
 		return this.detientTresor;
 	}
 	
+	/**
+	 * Méthode d'application sur le boolean qui gére l'aquisition du trésor par le personnage
+	 * @params boolean
+	 */
 	public void setDetientTresor(boolean set){
 		this.detientTresor = set;
 	}
+	
+	/**
+	 * Méthode d'affichage ToString
+	 * @return String resultat
+	 */
 	public String toString(){
 		return nom+" de l'équipe "+equipe+" possede "+energie+" points d'energie.";
 	}
 	
+	/**
+	 * attribut de class
+	 */
 	Object[] actionsSimples = { "haut", "bas", "gauche", "droite" };
 	Object[] actionsMulti = { "haut", "bas", "gauche", "droite", "haut gauche", "haut droite", "bas gauche",
 	"bas droite" };
-	
+
+	/**
+	 * Métode qui gére l'interface de déplacement
+	 * @return int resultat
+	 */
 	public int choixDeplacement(){
 		String deplacement;
 		if (this.nom != "voleur"){ // deplacement normal
