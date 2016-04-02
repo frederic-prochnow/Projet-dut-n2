c3ee9877f01c9d04ab8160d8235e44c81134ec1c
Frédéric PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-04-02 15:23:20 +0200

    Update Explorateur.java

--- src/Explorateur.java
+++ src/Explorateur.java
@@ -1,12 +1,17 @@
+/**
+* Class Explorateur
+* Class de création de l'explorateur
+* @author Team J3
+*/
 public class Explorateur extends Personnage {
 	
-	
-	
+	/**
+	 * Constructeur de la Class
+	 * Crée un explorateur
+	 * @params String nom, int equipe, int enregie,Position p,int type
+	 */
 	Explorateur(String nom, int equipe, int energie, Position p, int type){
 		super(nom,equipe,energie,p,type);
 	}
 	
-	
-	
-
 }
