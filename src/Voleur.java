ce91ed1f84ed64911bf42837175d0657045e2dc9
Frédéric PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-04-02 16:11:31 +0200

    Update Voleur.java

--- src/Voleur.java
+++ src/Voleur.java
@@ -1,17 +1,34 @@
-
-public class Voleur extends Personnage{
-
+/**
+ * Class Voleur
+ * Class gestion et cération de voleur
+ */
+ public class Voleur extends Personnage{
+	/**
+	 * atribut
+	 */
 	boolean cleVolee;
 	
+	/**
+	 * Constructeur de la calsss Voleur
+	 * Crée un voleur
+	 * @params String nom,int equipe,int energie, Position p,int type
+	 */
 	Voleur(String nom, int equipe, int energie, Position p, int type){
 		super(nom,equipe,energie,p,type);
 		this.cleVolee=false;
 	}
 	
+	/**
+	 * Méthode d'application sur le boolean gérant le vol de clef
+	 */
 	public void setCleVolee(){
 		this.cleVolee=true;
 	}
 	
+	/**
+	 * Méthode qui retourne un boolean gérant le vol de clef
+	 * @return boolean
+	 */
 	public boolean getCleVolee() {
 		return this.cleVolee;
 	}
