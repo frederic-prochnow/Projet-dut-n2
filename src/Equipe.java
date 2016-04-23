aa00b965e0b31e1b57e9c95d2e28b8d79efe19a5
Frédéric PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-04-23 15:29:27 +0200

    Update Equipe.java

--- src/Equipe.java
+++ src/Equipe.java
@@ -1,18 +1,42 @@
+/**
+ * Importation
+ */
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+/**
+ * Class Equipe
+ * Classe de gestion des Equipes
+ * @author Team J3
+ */
 public class Equipe {
+	
+	/**
+	 * Attribut
+	 */
 	private List<Personnage> liste;
 	
+	/**
+	 * Constructeur de classe
+	 */
 	public Equipe() {
 		liste = new ArrayList<Personnage>();
 	}
 	
+	/**
+	 * Recupere la liste de personnage
+	 * @return Liste de personnage
+	 */
 	public List<Personnage> getListe() {
 		return this.liste;
 	}
 	
+	/**
+	 * Recupere le personnage a une position donnee
+	 * @param x abscisse
+	 * @param y ordonnee
+	 * @return personnage concernee
+	 */
 	public Personnage getPersoSurPosition(int x, int y) {
 		Personnage temp;
 		for (Iterator<Personnage> perso = liste.iterator();perso.hasNext();) {
