90f7960379300635a378f0b8f457067326b991bb
Frédéric PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-04-23 15:04:57 +0200

    Update Clef.java

--- src/Clef.java
+++ src/Clef.java
@@ -1,20 +1,44 @@
+/**
+ * Class Clef
+ * Classe de gestion de la clef
+ * @author Team J3
+ */
 public class Clef {
+	/**
+	 * Attribut
+	 */
 	private Position pos;
 	private int type;
 	
+	/**
+	 * Constructeur de classe
+	 * @param position de la clef
+	 */
 	public Clef(Position p){
 		this.pos = p;
 		this.type = 8;
 	}
 	
+	/**
+	 * Retourne la position de la clef
+	 * @return position
+	 */
 	public Position getPos(){
 		return this.pos;
 	}
 	
+	/**
+	 * Retourne le type de l'élément
+	 * @return int type
+	 */
 	public int getType(){
 		return this.type;
 	}
 	
+	/**
+	 * Permet de modifier la position de la clef
+	 * @param position de la clef
+	 */
 	public void setPosition(Position p) {
 		this.pos = p;
 	}
